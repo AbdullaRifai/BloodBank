cc06f29917ca7d5ffbef99c0f126fa5b7a516ed2
Abdulla Rifai <rifaiabdulla7@gmail.com>
2020-02-15 15:14:12 -0500

    Folder restructure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from './components/NavBar Folder/Header.js';
+import Header from './components/Header/Header.js.js';
 
 function App() {
   return (
