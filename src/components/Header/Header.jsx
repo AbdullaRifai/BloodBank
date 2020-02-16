0375246e1e20b208946485b3b7b54a6bf3978cd3
Abdulla Rifai <rifaiabdulla7@gmail.com>
2020-02-15 19:01:43 -0500

    header in progress

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,25 @@
-import React, {useState} from 'react';
-
+import React from 'react';
+import './Header.scss';
+import {Nav, Col, Row, Container} from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div>
-            
+    <Nav defaultActiveKey="/home" className="justify-content-end" as="ul">
+        <Container>
+            <Col>
+                <Row>
+                    <Nav.Link href="/home">Active</Nav.Link>
+                </Row>
+                <Row>
+                    <Nav.Link eventKey="link-1">Link</Nav.Link>
+                </Row>
+                <Row>
+                    <Nav.Link eventKey="link-2">Link</Nav.Link>
+                </Row>
+            </Col>
+        </Container>
+    </Nav> 
         </div>
     )
 }
